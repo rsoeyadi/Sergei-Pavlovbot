0c9da6feca9e84aab2dae006332c99d5fc7f0d13
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2024-10-02 21:02:59 -0400

    msg history prints on_ready()

--- bot.py
+++ bot.py
@@ -13,7 +13,11 @@ client = discord.Client(intents=discord.Intents.all())
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
     print(f'{client.user} has connected to {guild.name}')
-
+    channel = discord.utils.get(guild.text_channels, name='general')
+    msgs = [msg async for msg in channel.history(limit=5, oldest_first=False)]
+    for msg in msgs:
+        print(msg.content, sep="\n")
+        
 @client.event
 async def on_member_join(member):
     channel = discord.utils.get(guild.text_channels, name='general')
@@ -21,4 +25,6 @@ async def on_member_join(member):
         await channel.send(
             f'{member.name}, do you want that thing to go from coast to coast? Or is it fine the way it is, being Dijkstra?'
         )
+
 client.run(TOKEN)
+
